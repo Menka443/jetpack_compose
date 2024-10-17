f35a811716ec74d24f6b4167e9df7b3f5803691e
Menka <menkashah060@gmail.com>
2024-10-17 22:37:54 +0530

    first commit

---
+++ Learn_Compose/app/src/main/java/com/example/learn_compose/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.learn_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.learn_compose.ui.theme.Learn_ComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+}
+
